3cfd38f410f26d5400ba77277477da83f3fe3bf0
Marah Orfahli <marahorfahli7@gmail.com>
2026-07-22 00:55:23 +0300

    Adding Fontawesome, tailwind Libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import "./App.css";
 
-const App = () =>{
+const App = () => {
   return (
     <>
-      Hello World!
-    </>
-  )
-}
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello World!
+      </h1>
 
-export default App
+      <FontAwesomeIcon icon={faHouse} size="2x" color="blue"/>
+      {/* spin */}
+    </>
+  );
+};
+
+export default App;
